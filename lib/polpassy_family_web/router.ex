468c90bea7fed89e8595b3ab8634b741206b7fc3
Henry Ohanga <ohanga.henry@gmail.com>
2019-05-29 19:20:00 +0300

    protect users resourse with authentication

--- lib/polpassy_family_web/router.ex
+++ lib/polpassy_family_web/router.ex
@@ -6,10 +6,33 @@ defmodule PolpassyFamilyWeb.Router do
     plug :fetch_session
   end
 
-  scope "/api", PolpassyFamilyWeb do
+  pipeline :api_auth do
+    plug :ensure_authenticated
+  end
+
+  scope "api", PolpassyFamilyWeb do
     pipe_through :api
 
-    resources "/users", UserController, except: [:new, :edit]
     post "/users/sign-in", UserController, :sign_in
+  end
+
+  scope "/api", PolpassyFamilyWeb do
+    pipe_through [:api, :api_auth]
+
+    resources "/users", UserController, except: [:new, :edit]
+  end
+
+  # plug function
+  defp ensure_authenticated(conn, _opts) do
+    current_user_id = get_session(conn, :current_user_id)
+
+    if current_user_id do
+      conn
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> render(PolpassyFamilyWeb.ErrorView, "401.json", message: "Unauthenticated user")
+      |> halt()
+    end
   end
 end
